37cbba4f7997f5fbf044908483f9cff8f8a908df
mythical <cameronbodie@gmail.com>
2021-01-25 13:15:00 -0600

    Finish user signup system & cleanup

--- src/db/users.rs
+++ src/db/users.rs
@@ -1,14 +1,38 @@
 use diesel::*;
 use super::establish_connection;
-use super::models::User;
+use super::models::{User, NewUser};
+use std::env;
 
-
-pub fn find_with_email(u_email:String) -> Result<Vec<User>, diesel::result::Error>{
+// make sure you use the & operator so we don't take ownership out of scope!
+pub fn find_with_email(u_email:&String) -> Result<Vec<User>, diesel::result::Error>{
     use super::schema::users::dsl::*;
-    use super::models::User;
     let conn = establish_connection();
     let user = users.filter(email.eq(u_email)).load::<User>(&conn)?;
     Ok(user)
 }
 
-pub fn insert()
+pub fn insert(first_name:&String, last_name:&String, email:&String, password:&String ) -> Result<bool, diesel::result::Error> {
+    // debugger says something about redundent import. Not sure why. Dont delete this line!
+    use argon2::{self, Config};
+    dotenv::dotenv().ok();
+
+    let pass_salt = env::var("HASH_PASSWORD_SALT").expect("COULD NOT FIND SALT HAS ENV");
+    let config = Config::default();
+    let password = argon2::hash_encoded(password.as_ref(), pass_salt.as_ref(), &config).unwrap();
+    println!("Hasshed password: {}", &password);
+
+
+    let conn = establish_connection();
+    use crate::db::schema::users;
+    let new_user:NewUser = NewUser{ first_name, last_name, email, password: &*password };
+    match diesel::insert_into(users::table).values(&new_user).execute(&conn)
+    {
+        Ok(_) => {
+            Ok(true)
+        }
+        Err(err) => {
+           Err(err)
+        }
+    }
+
+}
--- src/routes/users.rs
+++ src/routes/users.rs
@@ -21,7 +21,7 @@ pub fn signup_p(u_data:Form<NewUser>) -> Template{
         message: "Success".to_string(),
         ok: true
     };
-    let user = crate::db::users::find_with_email(u_data.into_inner().email);
+    let user = crate::db::users::find_with_email(&u_data.email);
     match user {
         Ok(u) => {
             if u.len() > 0 {
@@ -29,23 +29,28 @@ pub fn signup_p(u_data:Form<NewUser>) -> Template{
                 context.message = "User already exsits!".to_string();
             } else {
                 if u_data.password.len() > 7  && u_data.password.len() < 17 {
-
+                    match crate::db::users::insert(&u_data.first_name, &u_data.last_name, &u_data.email, &u_data.password) {
+                        Ok(_) => {
+                            context.ok = true;
+                            context.message = String::from("Successfully signed you up!");
+                        }
+                        Err(err) => {
+                            println!("Error in user route: {}",err);
+                            context.ok= false;
+                            context.message = String::from("Hmmm... Sometimes not quite right! Please try again later....")
+                        }
+                    };
 
                 } else {
-                    contest.message = "That password is too short! It must be between 8-16"
+                    context.ok = false;
+                    context.message = "That password is too short! It must be between 8-16".parse().unwrap()
                 }
             }
         }
         Err(err) =>  {
-            match err {
-                diesel::NotFound => {
-                    context.message = "Uesr not found in db. REay to add".to_string()
-                    // TODO: add function to add user to db
-                }
-                other_error => {
-                    println!("Some other error occured: {}", other_error)
-                }
-            }
+            println!("{}", err);
+            context.ok = false;
+            context.message = "db error".to_string();
         }
     }
     let context:Context = Context{context};
